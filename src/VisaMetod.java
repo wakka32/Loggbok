d3eda4fbd6a8aa55cf15a35399726739c8ebce00
william.grenback <william.grenback@elev.ga.ntig.se>
2019-09-03 09:32:55 +0200

    Initial commit

---
+++ src/VisaMetod.java
@@ -0,0 +1,11 @@
+public class loggbok {
+    public static void main (String[] args) {
+        printMenu();
+
+
+    }
+
+    private static void main printMenu() {
+        System.out.println();
+    }
+}
